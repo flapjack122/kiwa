b3c9522eb2d84e04d199d1c43839f5c1e5e9648b
islam <islam@yahoo.com>
2018-09-22 18:16:47 +0300

    Added all Sections

--- js/custom.js
+++ js/custom.js
@@ -5,4 +5,22 @@ $(function () {
 	//Adjust Main Height 
 	$('.main, .main .info, .main .content').height($(window).height());
 
+	// Add Height To Class box / ch 
+	var
+		boxMrTotal = $('.box').outerHeight(true) - $('.box').outerHeight(), // Height Of Margin
+		boxHeight = $('.sections').height() - boxMrTotal; // Height - Margin
+	$('.box, .ch').height(boxHeight); // Height Of Div
+
+
+	// Adust Contents On Screen Height Resize
+	$(window).resize(function () {
+		$('.main, .main .info, .main .content').height($(window).height()); // Adjust Class Main Height 
+		boxMrTotal = $('.box').outerHeight(true) - $('.box').outerHeight(); //Calc Class box Height 
+		$('.box').height($('.sections').height() - boxMrTotal); // Add Height To Class box
+	});
+
+
+
+
+
 });
